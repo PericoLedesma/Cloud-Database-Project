8b017153966b2eea79eba4b3373c545e07be6460
Pedro Ledesma <67062845+PericoLedesma@users.noreply.github.com>
2023-05-05 19:03:06 +0200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+# Dockerfile: blueprint to build the image
+# Image is a template to running containers
+# Container is the actual process
+
+
+# Base image
+FROM python:3.8.2-alpine
+
+
+# Inside container: make a new folder inside container
+WORKDIR /milestone
+
+# Second dot to say in the current container
+COPY client.py .
+
+# Outside container --> Inside container: copy from outside to inside.
+#This file has the libraries we want to install
+#  adds files from your Docker client’s current directory.
+COPY requirements.txt .
+#COPY . .
+
+
+# Inside container: install the python libraries used for the app
+#RUN builds your application with make.
+RUN pip3 install -r requirements.txt
+
+# Install any needed packages specified in requirements.txt
+#RUN pip install --no-cache-dir --trusted-host pypi.python.org -r requirements.txt
+
+
+# Outside container -> Inside container
+# When you run an image and generate a container, you add a new writable layer, also called the container layer, on top of the underlying layers.
+# All changes made to the running container, such as writing new files, modifying existing files, and deleting files, are written to this writable container layer.
+# . means everything int eh current directory
+# First period . -/backend-flask (outside container)
+# Second period . /backend-flask (inside container)
+COPY . .
+
+# Enviroment variables
+#ENV FLASK_DEBUG=1
+#ENV PORT=8080
+
+# Where to run it
+#EXPOSE ${PORT}
+
+# CMD (command)
+# To run flask.  python3 -m flask run --host=0.0.0.0 --port=4567
+# CMD specifies what command to run within the container.
+CMD [ "python", "./main.py" ]
+
+
+#CMD [ "python", "./helloworld.py"]
---
+++ client.py
@@ -0,0 +1,151 @@
+import socket
+import sys
+# import nltk
+# import threading
+
+# ------------------------------------------------------------------------
+
+class Client():
+    def __init__(self):
+        self.address_family = socket.AF_INET
+        self.socket_type = socket.SOCK_STREAM
+        self.sock = socket.socket(self.address_family, self.socket_type)
+        self.connected = False
+
+    def connect(self, server, port):
+        if self.connected is False:
+            self.port = port
+            self.server = server
+            self.sock.connect((self.server, self.port))
+            self.connected = True
+            #Todo Note, if the connection establishment failed, the client application should provide a useful error message to the user
+            return(f"Connection to MSRG Echo server established: / {self.server} /  {self.port}")
+        else:
+            return("Already connected")
+
+    def disconnect(self):
+        try:
+            self.sock.close()
+            self.connected = False
+            return(f"Connection terminated: /{self.server} / {self.port}")
+            #Todo Note that the connection might also be lost due to a connection error or a breakdown of the server
+        except:
+            return("No connection active")
+
+    def send_messages(self, tokens):
+        if self.connected is True:
+            delimiter = ' '
+            message = delimiter.join(tokens)
+            self.sock.sendall(message.encode())
+
+            # data = self.sock.recv(1024)  # Number of bytes we are waiting
+            # buffer = data.decode()
+            # return (f"Received: {buffer}")
+            return self.receive_message()
+        else:
+            return("No connection active")
+
+    def receive_message(self):
+        data = self.sock.recv(1024)  # Number of bytes we are waiting
+        buffer = data.decode()
+        return (buffer.strip())
+
+
+    def listener(self): # For the future, not active
+        self.listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener_socket.bind('localhost', 1234)
+        self.listener_socket.listen(1)
+        connection, client_address = self.listener_socket.accept()
+        print('Accepted connection from {}:{}'.format(*client_address))
+        # receive messages from the client
+        while True:
+            try:
+                data = self.sock.recv(1024)
+                if data:
+                    print("Listener --> ", data.decode('utf-8'))
+            except:
+                break
+# ------------------------------------------------------------------------
+class Command_line_shell():
+    def __init__(self):
+        self.cli_output("Welcome to the CLI of the client")
+
+    def command_help(self):
+        self.cli_output("Welcome to the CLI. Available commands: ")
+        print("\tconnect: to connect to a server. Arg: Server+port")
+        print("\tdisconnect: to disconnect from a server")
+        print("\tsend: to send a message. Arg: the message")
+        print("\tlog: on process")
+        print("\thelp: displays this message")
+        print("\tquit: quits the program")
+
+    def command_exit(self):
+        self.cli_output("Goodbye!")
+        sys.exit(0)
+
+    def cli_output(self, *args):
+        if len(args) != 0:
+            sys.stdout.write(f"EchoClient> {' '.join(str(arg) for arg in args)}")
+            sys.stdout.write('\n')
+
+
+    def user_input(self):
+        # cli_input = sys.stdin.readline().strip()
+        # sys.stdout.write('EchoClient>')
+        # print("primero")
+        # cli_input = sys.stdin.readline()
+        # print("Command", cli_input)
+        cli_input = input("EchoClient>").split()
+        token_list = []
+        for word in cli_input:
+            token_list.append(word)
+
+        self.command = token_list
+        print("Tokens:", self.command)
+
+
+# ----------------------------------------------------------------------------
+def main():
+    CLI = Command_line_shell()
+    socket_to_server = Client()
+    # For the futurew
+    # receive_thread = threading.Thread(target=socket_to_server.listener)
+    # receive_thread.start()
+
+    while True:
+        CLI.user_input() # List of tokens
+
+        if CLI.command[0] == "connect":
+            if len(CLI.command) != 3:
+                CLI.cli_output("connect command needs 2 arguments: server + port")
+            elif len(CLI.command) == 3:
+                CLI.cli_output(socket_to_server.connect('cdb.dis.cit.tum.de', 5551))
+                CLI.cli_output(socket_to_server.receive_message())
+
+        elif CLI.command[0] == "disconnect":
+            if len(CLI.command)== 1:
+                CLI.cli_output(socket_to_server.disconnect())
+            elif len(CLI.command) != 1:
+                CLI.cli_output("disconnect command doesn't accepts arguments")
+
+        elif CLI.command[0] == "send":
+            if len(CLI.command) > 1:
+                # CLI.cli_output(socket_to_server.listen())
+                CLI.cli_output(socket_to_server.send_messages(CLI.command[1:]))
+            elif len(CLI.command) == 1:
+                CLI.cli_output("send command needs 1 argument at least")
+
+        elif CLI.command[0] == "logLevel":
+            pass
+        elif CLI.command[0] == "help":
+            CLI.command_help()
+        elif CLI.command[0] == "quit":
+            CLI.command_exit()
+        else:
+            CLI.cli_output("Invalid command. Type 'help' for a list of available commands.")
+
+# ----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
+
